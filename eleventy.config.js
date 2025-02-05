95dbd4f1c795ba1d9cce34a9e8658eb5d7ff3877
jayden626 <jayden_626@hotmail.com>
2025-02-05 13:52:50 +1100

    removed bootstrap, added basic styles

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,18 +1,24 @@
-import { eleventyImageTransformPlugin, Image } from "@11ty/eleventy-img";
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 
 export default function (eleventyConfig) {
 
 	eleventyConfig.addPlugin(eleventyImageTransformPlugin);
+
 	eleventyConfig.addShortcode('excerpt', post => extractExcerpt(post));
+
 	eleventyConfig.addPassthroughCopy('src/javascript/*')
+	eleventyConfig.addWatchTarget('src/javascript/*')
+	eleventyConfig.addPassthroughCopy('src/styles/*')
+	eleventyConfig.addWatchTarget('src/styles/*')
 	eleventyConfig.addPassthroughCopy('src/posts/*/*/images/*')
 
 	function extractExcerpt(post) {
+		const maxCharacters = 200;
 		if(!post.templateContent) return '';
 		if(post.templateContent.indexOf('</p>') > 0) {
             const start = post.templateContent.indexOf('<p>');
 			const end = post.templateContent.indexOf('</p>');
-			return `${post.templateContent.substr(start, Math.min(end+4,100)).substr(0, ).trim()}${end > 100 ? "..." : ""}`;
+			return `${post.templateContent.substr(start, Math.min(end+4,maxCharacters)).trim()}${end > maxCharacters ? "..." : ""}`;
 		}
 		return post.templateContent;
 	}
@@ -21,14 +27,6 @@ export default function (eleventyConfig) {
 	eleventyConfig.addFilter("niceDate", function(d) {
 		return dateFormatter.format(d);
 	});
-
-	// eleventyConfig.addLiquidShortcode("svgIcon", async filename => {
-	// 	const metadata = await Image(`./src/_includes/icons/${filename}.svg`, {
-	// 		formats: ["svg"],
-	// 		dryRun: true,
-	// 	});
-	// 	return metadata.svg[0].buffer.toString();
-	// })
 
 	return {
 		dir: {
